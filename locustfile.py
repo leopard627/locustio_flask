ef2cf6d979edfd685536e1bf301481cb5c4eb1b8
elastic <elastic7327@gmail.com>
2017-11-12 21:30:02 +0900

    first cmt

---
+++ locustfile.py
@@ -0,0 +1,10 @@
+from locust import HttpLocust, TaskSet, task
+
+class UserBehavior(TaskSet):
+
+    @task
+    def get_something(self):
+        self.client.get("/something")
+
+class WebsiteUser(HttpLocust):
+    task_set = UserBehavior
